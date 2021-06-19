e5b14196f54df6a5a783c0de1a24fec3ba9b2853
Andreas S K <andreas.syaloom.k@gmail.com>
2021-06-19 22:16:50 +0700

    acl

---
+++ data/Payroll.cob
@@ -0,0 +1,9 @@
+       01  SAMPLE-PAYROLL-FILE.                                     
+        03  PAYROLL-INFO.                                        
+         05  EMPNO                        PIC X(6).            
+         05  GROSS_PAY                    PIC S9(15)V9(2) COMP-3.        
+         05  TAX_AMOUNT                   PIC S9(15)V9(2) COMP-3.
+         05  NET_PAY                      PIC S9(15)V9(2) COMP-3.
+         05  WORKDEPT                     PIC X(3).
+         05  PAY_DATE                     PIC X(8).
+         05  CHEQUE_NO                    PIC X(5).
